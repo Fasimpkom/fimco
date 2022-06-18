658715bc4bc2026bdd5708f9d00ea956031d35f3
betester <ikramzulkarnaen02@gmail.com>
2022-06-18 15:25:33 +0700

    add detail on card

--- app/core/components/Card.tsx
+++ app/core/components/Card.tsx
@@ -1,29 +1,42 @@
 import { ReactNode } from "react"
 import { FC } from "react"
 import { Image } from "next/image"
+import { Link } from "blitz"
 
 type CardProps = {
   thumbnail?: string
   title: string
   content: string
   children?: ReactNode
+  detail?: string
 }
 
-const Card: FC<CardProps> = ({ title, thumbnail, content, children }) => {
+const Card: FC<CardProps> = ({ title, thumbnail, content, children, detail }) => {
   return (
-    <div className="flex bg-white rounded p-5 min-w-[350px] justify-between">
-      {/* thumbnail */}
-      <div className="flex">
-        {thumbnail && <Image src={thumbnail} alt="a picture"></Image>}
-        {/* title */}
-        <div className="ml-3">
-          <div className="text-gray-400">{title}</div>
-          {/* content */}
-          <div className="font-bold">{content}</div>
+    <div className="bg-white rounded min-w-[350px]">
+      <div className="flex justify-between m-5">
+        {/* thumbnail */}
+        <div className="flex">
+          {thumbnail && <Image src={thumbnail} alt="a picture"></Image>}
+          {/* title */}
+          <div className="ml-3">
+            <div className="text-gray-400">{title}</div>
+            {/* content */}
+            <div className="font-bold">{content}</div>
+          </div>
         </div>
+        {/* action */}
+        <div>{children || ""}</div>
       </div>
-      {/* action */}
-      <div>{children || ""}</div>
+      {detail && (
+        <div className="mt-4 bg-gray-200 rounded-bl rounded-br">
+          <div className="p-3 ml-3">
+            <Link href={"google.com"}>
+              <a className="text-blue-600 font-bold">Lihat Selengkapnya</a>
+            </Link>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
--- app/pages/render_component.tsx
+++ app/pages/render_component.tsx
@@ -5,7 +5,9 @@ import { BlitzPage } from "blitz"
 const RenderComponent: BlitzPage = () => {
   return (
     <div className="flex justify-center h-[100vh] items-center bg-black">
-      <Card title="Pengeluaran" content="100.000 Rp"></Card>
+      <Card title="Pengeluaran" content="100.000 Rp">
+        <Button>asd</Button>
+      </Card>
     </div>
   )
 }
