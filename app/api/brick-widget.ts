436d0840d0857c1bf7372e086c36da887d522324
Taufik Pragusga <taufik@pragusga.com>
2022-06-19 19:33:18 +0700

    fix: fix eslint

--- app/api/brick-widget.ts
+++ app/api/brick-widget.ts
@@ -10,26 +10,26 @@ const handler = async (req: BlitzApiRequest, res: BlitzApiResponse) => {
   console.log("HELLLLOOO")
   if (req.method !== "POST") return res.status(405).json({ error: "Method not allowed" })
 
-  const data: BrickWidgetBody[] = req.body
-  for (const d of data) {
-    console.log(d)
-    await db.walletAccount.upsert({
-      where: {
-        bankId_userId: {
-          bankId: Number(d.bankId),
-          userId: d.userId,
-        },
-      },
-      update: {
-        accessToken: d.accessToken,
-      },
-      create: {
-        bankId: Number(d.bankId),
-        userId: d.userId,
-        accessToken: d.accessToken,
-      },
-    })
-  }
+  // const data: BrickWidgetBody[] = req.body
+  // for (const d of data) {
+  //   console.log(d)
+  //   await db.walletAccount.upsert({
+  //     where: {
+  //       bankId_userId: {
+  //         bankId: Number(d.bankId),
+  //         userId: d.userId,
+  //       },
+  //     },
+  //     update: {
+  //       accessToken: d.accessToken,
+  //     },
+  //     create: {
+  //       bankId: Number(d.bankId),
+  //       userId: d.userId,
+  //       accessToken: d.accessToken,
+  //     },
+  //   })
+  // }
   return res.status(200).send("OK")
 }
 
